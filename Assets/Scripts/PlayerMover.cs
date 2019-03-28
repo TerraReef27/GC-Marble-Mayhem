89df6537c7b1086f6440139577b62444a7229e43
TerraReef27 <47617938+TerraReef27@users.noreply.github.com>
2019-03-27 22:11:54 -0400

    Meeting 2 Changes

---
+++ Assets/Scripts/PlayerMover.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMover : MonoBehaviour
+{
+    Rigidbody myRigidbody;
+    Transform parent;
+
+    public float maxPower = 2000f;
+    [SerializeField] float chargeRate = 10f;
+    [SerializeField] float decreaseRate = 1f;
+    [Range(0f, 2000f)]
+    public float power;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+        parent = GetComponentInParent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveWithButtons();
+        MoveWithMouse();
+    }
+
+    private void MoveWithMouse()
+    {
+        if(Input.GetKey(KeyCode.Mouse0))
+        {
+            Charge();
+        }
+        if(Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            myRigidbody.angularDrag = 0.05f;
+            
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit))
+            {
+                Vector3 moveDirection = hit.point - transform.position;
+                moveDirection.y = 0;
+                moveDirection.Normalize();
+
+                //TODO remove later
+                Debug.DrawLine(transform.position, hit.point);
+
+                myRigidbody.AddForceAtPosition(moveDirection * power, parent.position);
+                power = 0;
+            }
+        }
+    }
+
+    private void Charge()
+    {
+        if(power < maxPower)
+        {
+            power +=  chargeRate;
+        }
+
+        myRigidbody.angularDrag += decreaseRate;
+    }
+
+    private void MoveWithButtons()
+    {
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            myRigidbody.AddForceAtPosition(Vector3.right * Input.GetAxis("Horizontal") * power, parent.position);
+        }
+        if (Input.GetAxis("Vertical") != 0)
+        {
+            myRigidbody.AddForceAtPosition(Vector3.forward * Input.GetAxis("Vertical") * power, parent.position);
+        }
+    }
+}
---
+++ Assets/Scripts/UISliderScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+public class UISliderScript : MonoBehaviour
+{
+    public Slider slider;
+    private PlayerMover mover;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mover = FindObjectOfType<PlayerMover>();
+        slider.maxValue = mover.maxPower;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        slider.value = mover.power;
+    }
+}

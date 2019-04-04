79e0e5ee0a80722f82df5c16f765ddf90d80f2f9
TerraReef27 <47617938+TerraReef27@users.noreply.github.com>
2019-04-03 22:37:19 -0400

    Meeting 3 Changes
    Mostly just UI stuff with some script changes and additions

--- Assets/Scripts/PlayerMover.cs
+++ Assets/Scripts/PlayerMover.cs
@@ -7,6 +7,7 @@ public class PlayerMover : MonoBehaviour
 {
     Rigidbody myRigidbody;
     Transform parent;
+    ParticleSystem particles;
 
     public float maxPower = 2000f;
     [SerializeField] float chargeRate = 10f;
@@ -17,6 +18,7 @@ public class PlayerMover : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        particles = GetComponentInChildren<ParticleSystem>();
         myRigidbody = GetComponent<Rigidbody>();
         parent = GetComponentInParent<Transform>();
     }
@@ -24,8 +26,8 @@ public class PlayerMover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        MoveWithButtons();
         MoveWithMouse();
+        particles.transform.rotation = Quaternion.Euler(-90f, 0.0f, gameObject.transform.rotation.z * -1.0f);
     }
 
     private void MoveWithMouse()
@@ -33,6 +35,11 @@ public class PlayerMover : MonoBehaviour
         if(Input.GetKey(KeyCode.Mouse0))
         {
             Charge();
+        }
+        else
+        {
+            //TODO fix so not on update
+            particles.Stop();
         }
         if(Input.GetKeyUp(KeyCode.Mouse0))
         {
@@ -64,17 +71,12 @@ public class PlayerMover : MonoBehaviour
         }
 
         myRigidbody.angularDrag += decreaseRate;
+
+        if (!particles.isPlaying)
+        {
+            particles.Play();
+        }
     }
 
-    private void MoveWithButtons()
-    {
-        if (Input.GetAxis("Horizontal") != 0)
-        {
-            myRigidbody.AddForceAtPosition(Vector3.right * Input.GetAxis("Horizontal") * power, parent.position);
-        }
-        if (Input.GetAxis("Vertical") != 0)
-        {
-            myRigidbody.AddForceAtPosition(Vector3.forward * Input.GetAxis("Vertical") * power, parent.position);
-        }
-    }
+
 }
